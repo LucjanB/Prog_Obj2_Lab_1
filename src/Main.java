2ecb0c10d1dc97aa051e9c2c8f7c33f86cf92f13
bajurski <bajurski@hotmail.com>
2022-10-25 00:35:19 +0200

    First commit.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Calculations calc = new Calculations();
+        Visual vis = new Visual();
+        vis.showInputPane();
+        //System.out.println("Hello world!");
+
+    }
+}
---
+++ src/Visual.java
@@ -0,0 +1,9 @@
+
+import java.util.*;
+import javax.swing.JOptionPane;
+public class Visual {
+    void showInputPane(){
+        String myInputStr = JOptionPane.showInputDialog("This is my message !!!","This is already here.");
+        System.out.println(myInputStr);
+    }
+}
