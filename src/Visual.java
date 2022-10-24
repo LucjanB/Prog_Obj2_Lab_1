d107e73c6e61dd4006b2f812450df44d3ded1908
bajurski <bajurski@hotmail.com>
2022-10-25 00:37:12 +0200

    First commit.

--- src/Visual.java
+++ src/Visual.java
@@ -3,7 +3,7 @@ import java.util.*;
 import javax.swing.JOptionPane;
 public class Visual {
     void showInputPane(){
-        String myInputStr = JOptionPane.showInputDialog("This is my message !!!","This is already here.");
+        String myInputStr = JOptionPane.showInputDialog("This is my message !","This is already here.");
         System.out.println(myInputStr);
     }
 }
