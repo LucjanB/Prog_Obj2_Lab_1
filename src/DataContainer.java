4b9d19e157c8b3b890f302695799510282fca6f1
bajurski <bajurski@hotmail.com>
2022-10-28 02:23:55 +0200

    Clean up.

--- src/DataContainer.java
+++ src/DataContainer.java
@@ -1,22 +1,12 @@
 public record DataContainer(int prodCount, float netPrice, float vat, float vatVal, int numOfItems, float totalNet) {
-
-    public DataContainer(int prodCount, float netPrice, float vat, float vatVal, int numOfItems, float totalNet) {
-        this.vatVal = vatVal;
-        this.prodCount = prodCount;
-        this.netPrice = netPrice;
-        this.vat = vat;
-        this.numOfItems = numOfItems;
-        this.totalNet = totalNet;
-    }
-
     @Override
     public String toString() {
         System.out.println("Product No : "+ prodCount);
-        System.out.println("Number of items : "+ numOfItems);
-        System.out.println("Net price : "+ netPrice);
+        System.out.println("Number of items : " + numOfItems);
+        System.out.println("Net price of single product: "+ netPrice);
         System.out.println("VAT % : "+ vat +" %");
         System.out.println("Total VAT value : " + vatVal);
-        System.out.println("Total net : "+ totalNet);
+        System.out.println("Total net price : " + totalNet);
         return "";
     }
 }
--- src/Visual.java
+++ src/Visual.java
@@ -1,5 +1,5 @@
-
 import javax.swing.JOptionPane;
+
 public class Visual {
     void showMessageDialog(){JOptionPane.showMessageDialog(null,"Bad input !","ERROR",JOptionPane.ERROR_MESSAGE);}
     String showInputDialog(String info) {
