2290d1cdeeaa115a9fc90a6a9de30107a6f69a38
bajurski <bajurski@hotmail.com>
2022-10-28 02:08:56 +0200

    All components are working. Testing and clean up needed.

--- src/DataContainer.java
+++ src/DataContainer.java
@@ -1,6 +1,7 @@
-public record DataContainer(int prodCount,float netPrice, float vat, int numOfItems, float totalNet) {
+public record DataContainer(int prodCount, float netPrice, float vat, float vatVal, int numOfItems, float totalNet) {
 
-    public DataContainer(int prodCount,float netPrice, float vat, int numOfItems, float totalNet) {
+    public DataContainer(int prodCount, float netPrice, float vat, float vatVal, int numOfItems, float totalNet) {
+        this.vatVal = vatVal;
         this.prodCount = prodCount;
         this.netPrice = netPrice;
         this.vat = vat;
@@ -11,10 +12,11 @@ public record DataContainer(int prodCount,float netPrice, float vat, int numOfIt
     @Override
     public String toString() {
         System.out.println("Product No : "+ prodCount);
-        System.out.println("Net : "+ netPrice);
-        System.out.println("VAT : "+ vat);
-        System.out.println("Number : "+ numOfItems);
-        System.out.println("Total  : "+ totalNet);
+        System.out.println("Number of items : "+ numOfItems);
+        System.out.println("Net price : "+ netPrice);
+        System.out.println("VAT % : "+ vat +" %");
+        System.out.println("Total VAT value : " + vatVal);
+        System.out.println("Total net : "+ totalNet);
         return "";
     }
 }
